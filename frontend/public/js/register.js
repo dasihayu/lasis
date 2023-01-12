cc2b6c764cb2359054888135e648063e1dba860e
YordanAseramaLuturyali <yordanluturyali@gmail.com>
2023-01-12 11:57:31 +0700

    mwubal

--- frontend/public/js/register.js
+++ frontend/public/js/register.js
@@ -2,7 +2,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBK2A--fVMgOZ3Eh3JANV9dFxWR_cwn44E",
   authDomain: "lapaksiswa-1dbf5.firebaseapp.com",
-  databaseURL: "https://lapaksiswa-1dbf5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://lapaksiswa-1dbf5-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "lapaksiswa-1dbf5",
   storageBucket: "lapaksiswa-1dbf5.appspot.com",
   messagingSenderId: "435863796587",
@@ -18,26 +18,26 @@ const btnDaftar = document.getElementById('btndaftar');
 
 
 btnDaftar.addEventListener('click', () => {
-  
+
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const namaDepan = document.getElementById('namadepan').value;
   const namaBelakang = document.getElementById('namabelakang').value;
-  
+
   firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    // ...
-    
-    firebase.database().ref('users/' + user.uid).set({
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+
+      firebase.database().ref('users/' + user.uid).set({
         'nama depan': namaDepan,
         'nama belakang': namaBelakang,
         email: email,
         pass: password
       })
-      
-      alert('Berhasil Membuat Akun');
+
+      alert("Berhasil Membuat Akun");
     })
     .catch((error) => {
       var errorCode = error.code;
--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -8,3 +8,17 @@ function toggle() {
     state = true;
   }
 }
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
